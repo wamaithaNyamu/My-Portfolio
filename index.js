16aa2db4f0303ed07ba886664a39454660dc03ea
wamaitha <devwamaitha@gmail.com>
2019-06-09 16:45:45 +0300

    model and name

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xffffff );
+// scene.background = new THREE.Color( 0xffffff );
 			var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 			var renderer = new THREE.WebGLRenderer();
@@ -19,28 +19,72 @@ scene.background = new THREE.Color( 0xffffff );
 			scene.add(fillLight);
 			scene.add(backLight);
 
-			camera.position.z = 5;
-	
-			var mtlLoader = new THREE.MTLLoader();
-			mtlLoader.setTexturePath('Assets/');
-			mtlLoader.setPath('Assets/');
-			mtlLoader.load('sa.mtl', function (materials) {
 			
-				materials.preload();
+			// var mtlLoader = new THREE.MTLLoader();
+			// mtlLoader.setTexturePath('Assets/');
+			// mtlLoader.setPath('Assets/');
+			// mtlLoader.load('sa.mtl', function (materials) {
 			
-				var objLoader = new THREE.OBJLoader();
-				objLoader.setMaterials(materials);
-				objLoader.setPath('Assets/');
-				objLoader.load('sa.obj', function (object) {
+			// 	materials.preload();
 			
-					scene.add(object);
+			// 	var objLoader = new THREE.OBJLoader();
+			// 	objLoader.setMaterials(materials);
+			// 	objLoader.setPath('Assets/');
+			// 	objLoader.load('sa.obj', function (object) {
+			
+			// 		scene.add(object);
 		
-				});
+			// 	});
 			
-			});
+			// });
+			const loader = new THREE.OBJLoader()
+			loader.load('https://cdn.glitch.com/fcf3c007-b4eb-4250-ba6b-653fdab94ce3%2Fjapanese_temple.obj?1558792651869',
+			(obj) => {
+				// the request was successfull
+				let material = new THREE.PointsMaterial({ color: 'grey', size: 0.25 })
+				mesh = new THREE.Points(obj.children[0].geometry, material)
+				mesh.position.y = -15 //this model is not exactly in the middle by default so I moved it myself
+				scene.add(mesh)
+
+			},
+			(xhr) => {
+				// the request is in progress
+				console.log(xhr)
+			},
+			(err) => {
+				// something went wrong
+				console.error("loading .obj went wrong, ", err)
+			})
+
+			camera.position.z = 80;
+			const controls = new THREE.OrbitControls(camera)
+  			controls.enableZoom = false //zoom happens on scroll which can be quite annoying
+
+	
+
+
+			// var mtlLoader2 = new THREE.MTLLoader();
+			// mtlLoader2.setTexturePath('obj/');
+			// mtlLoader2.setPath('obj/');
+			// mtlLoader2.load('saucer.mtl', function (materials) {
+			
+			// 	materials.preload();
+			
+			// 	var objLoader2 = new THREE.OBJLoader();
+			// 	objLoader2.setMaterials(materials);
+			// 	objLoader2.setPath('obj/');
+			// 	objLoader2.load('saucer.obj', function (object) {
+			
+			// 		scene.add(object);
+		
+			// 	});
+			
+			// });
 			
 			var animate = function () {
 				requestAnimationFrame( animate );
+				mesh.rotation.y += 0.01;
+				// mesh.rotation.y -= 0.01;
 
 				renderer.render( scene, camera );
 			};
---
+++ obj/ReadMe.txt
@@ -0,0 +1,7 @@
+UFO concept- 4
+
+Model Specs:
+
+Vert: 666,434
+
+Faces: 666,402
