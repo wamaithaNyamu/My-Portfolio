59931328f5ea48ef1599fa0bd88c5983b57107a6
wamaitha <devwamaitha@gmail.com>
2019-06-09 12:10:18 +0300

    loaded model

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
 var scene = new THREE.Scene();
-			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+scene.background = new THREE.Color( 0xffffff );
+			var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 			var renderer = new THREE.WebGLRenderer();
 			renderer.setSize( window.innerWidth, window.innerHeight );
 			document.body.appendChild( renderer.domElement );
 
-			var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			var cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
+			var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+			keyLight.position.set(-100, 0, 100);
+			
+			var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+			fillLight.position.set(100, 0, 100);
+			
+			var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+			backLight.position.set(100, 0, -100).normalize();
+			
+			scene.add(keyLight);
+			scene.add(fillLight);
+			scene.add(backLight);
 
 			camera.position.z = 5;
-
+	
+			var mtlLoader = new THREE.MTLLoader();
+			mtlLoader.setTexturePath('Assets/');
+			mtlLoader.setPath('Assets/');
+			mtlLoader.load('sa.mtl', function (materials) {
+			
+				materials.preload();
+			
+				var objLoader = new THREE.OBJLoader();
+				objLoader.setMaterials(materials);
+				objLoader.setPath('Assets/');
+				objLoader.load('sa.obj', function (object) {
+			
+					scene.add(object);
+		
+				});
+			
+			});
+			
 			var animate = function () {
 				requestAnimationFrame( animate );
 
-				cube.rotation.x += 0.01;
-				cube.rotation.y += 0.01;
-
 				renderer.render( scene, camera );
 			};
+
 
 			animate();
