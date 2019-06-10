9da3b55ef10f267932cd862bf64832cf43a05f03
wamaitha <devwamaitha@gmail.com>
2019-06-10 18:06:15 +0300

    refactored code

--- index.js
+++ index.js
@@ -1,164 +1,252 @@
-// // initialise constants
-// const numberOfParticles = 10000;
+// Options
+const numberOfParticles = 10000;
 		
-// const particleImage = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/605067/particle-tiny.png',
-// 			particleColor = '0xFFFFFF',
-// 			particleSize = .1;
+const particleImage = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/605067/particle-tiny.png',
+			particleColor = '0xFFFFFF',
+			particleSize = .1;
 
-// const defaultAnimationSpeed = 1,
-// 			morphAnimationSpeed = 3;
-// // select all paragraph tags in the span timeline class
+const defaultAnimationSpeed = 1,
+			morphAnimationSpeed = 3;
 
-// const sideNav = document.getElementsByClassName('spanTimeline')[0].querySelectorAll('p')
 
-// //initiate renderer
-// var renderer = new THREE.WebGLRenderer();
-// renderer.setPixelRatio(window.devicePixelRatio);
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild( renderer.domElement );
 
-// // Make the threejs canvas responsive
-// function fullScreen () {
-// 	camera.aspect = window.innerWidth / window.innerHeight;
-// 	camera.updateProjectionMatrix();
 
-// 	renderer.setSize( window.innerWidth, window.innerHeight );
-// }
 
-// window.addEventListener('resize', fullScreen, false)
 
-// // create the scene
-// var scene = new THREE.Scene();
+// Triggers
+const triggers = document.getElementsByClassName('triggers')[0].querySelectorAll('span')
 
-// // create camera and position it
-// var camera = new THREE.PerspectiveCamera(45, window.innerWidth/window/innerHeight, 1, 10000)
-// camera.position.y = 25;
-// camera.position.z = 36;
+var stats = new Stats();
+stats.showPanel(0);
+document.body.appendChild( stats.dom );
 
-// // particles variables
-// var particleCount = numberOfParticles;
-// let letterWPoints,
-// 	chairPoints,
-// 	anatomyPoints,
-// 	guitarPoints,
-// 	cameraPoints,
-// 	bookPoints,
-// 	padpoints,
-// 	phonePoints,
+// Renderer
+var renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-// var pMaterial = new THREE.Geometry(),
-// 	letterWParticles = new THREE.Geometry(),
-// 	chairParticles = new THREE.Geometry(),
-// 	anatomyParticles = new THREE.Geometry(),
-// 	guitarParticles = new THREE.Geometry(),
-// 	cameraParticles = new THREE.Geometry(),
-// 	bookParticles = new THREE.Geometry(),
-// 	padpoarticles= new THREE.Geometry(),
-// 	phoneParticles = new THREE.Geometry();
+// Ensure Full Screen on Resize
+function fullScreen () {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
 
-// var pMaterial = new THREE.PointCloudMaterial({
-// 	color: particleColor,
-// 	size:particleSize,
-// 	map: THREE.ImageUtils.loadTexture(particleImage),
-// 	blending:THREE.AdditiveBlending,
-// 	transparent: true
-// });
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
 
-// // custom obj i will use
+window.addEventListener('resize', fullScreen, false)
 
-// // initialise obj loader
-// var ObjLoader = new THREE.OBJLoader();
+// Scene
+var scene = new THREE.Scene();
 
-// // landing section
-// objLoader.load( 'https://cdn.glitch.com/6a29bec8-9471-403e-a502-375b8f18bbc2%2F1333%20Phone.obj?v=1560149942462', function ( object ) {	
-// 	object.traverse( function ( child ) {
-// 		if ( child instanceof THREE.Mesh ) {
-// 			let scale = .3;
+// Camera and position
+var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
+
+camera.position.y = 25;
+camera.position.z = 36;
+
+// Lighting
+var light = new THREE.AmbientLight( 0x404040, 1 ); // soft white light
+scene.add( light );
+
+// Orbit Controls
+var controls = new THREE.OrbitControls( camera );
+controls.update();
+
+// Particle Vars
+var particleCount = numberOfParticles;
+
+let 	projectPoints,
+		homePoints,
+		contactPoints,
+		aboutPoints;
+
+var particles = new THREE.Geometry(),
+		projectParticles = new THREE.Geometry(),
+		homeParticles = new THREE.Geometry(),
+		aboutParticles =  new THREE.Geometry(),
+		contactParticles = new THREE.Geometry();
+
+var pMaterial = new THREE.PointCloudMaterial({
+			color: particleColor,
+			size: particleSize,
+			map: THREE.ImageUtils.loadTexture(particleImage),
+			blending: THREE.AdditiveBlending,
+			transparent: true
+});
+
+// Custom (OGJ) Objects
+// PROJECTS
+var objLoader = new THREE.OBJLoader();
+objLoader.load( 'https://cdn.glitch.com/6a29bec8-9471-403e-a502-375b8f18bbc2%2F1307%20Xbox%20Controller.obj?v=1560150840338', function ( object ) {
+
+	object.traverse( function ( child ) {
+		if ( child instanceof THREE.Mesh ) {
+			let scale = 1;
 			
-// 			let area = new THREE.Box3();
-// 				area.setFromObject( child );
-// 			let yOffset = (area.max.y * scale) / 2;
-// 			// scene.background = new THREE.Color( 0xffff33 );
-
-// 			child.geometry.scale(scale,scale,scale);
-// 			rocketPoints = THREE.GeometryUtils.randomPointsInBufferGeometry(child.geometry, particleCount);
-// 			createVertices(rocketParticles, rocketPoints, yOffset, 2);
-// 		}
-// 	});
-// });
-// // var objLoader = new THREE.OBJLoader();
-// // objLoader.setPath(codepenAssetUrl);
-// objLoader.load( 'https://cdn.glitch.com/6a29bec8-9471-403e-a502-375b8f18bbc2%2FAcoustic_Guitar_01.obj?v=1560149442376', function ( object ) {
-// 	object.traverse( function ( child ) {
-// 		if ( child instanceof THREE.Mesh ) {
-// 			let scale = 1.5;
+			let area = new THREE.Box3();
+				area.setFromObject( child );
+			let yOffset = (area.max.y * scale) / 2;
+			child.geometry.scale(scale,scale,scale);
+			projectPoints = THREE.GeometryUtils.randomPointsInBufferGeometry(child.geometry, particleCount);
+			createVertices(projectParticles, projectPoints, yOffset, 2);
+		}
+	});
+});
+// CONTACTS
+objLoader.load( 'https://cdn.glitch.com/6a29bec8-9471-403e-a502-375b8f18bbc2%2F1333%20Phone.obj?v=1560149942462', function ( object ) {	
+	object.traverse( function ( child ) {
+		if ( child instanceof THREE.Mesh ) {
+			let scale = .3;
 			
-// 			let area = new THREE.Box3();
-// 				area.setFromObject( child );
-// 			let yOffset = (area.max.y * scale) / 2;
+			let area = new THREE.Box3();
+				area.setFromObject( child );
+			let yOffset = (area.max.y * scale) / 2;
 			
-// 			child.geometry.scale(scale,scale,scale);
-// 			spacemanPoints = THREE.GeometryUtils.randomPointsInBufferGeometry(child.geometry, particleCount);
-// 			createVertices(spacemanParticles, spacemanPoints, yOffset, 3);
-// 		}
-// 	});
-// });
+			child.geometry.scale(scale,scale,scale);
+			contactPoints = THREE.GeometryUtils.randomPointsInBufferGeometry(child.geometry, particleCount);
+			createVertices(contactParticles, contactPoints, yOffset, 3);
+		}
+	});
+});
 
-// // Particles
-// for (var p = 0; p < particleCount; p++) {
-// 	var vertex = new THREE.Vector3();
-// 			vertex.x = 0;
-// 			vertex.y = 0;
-// 			vertex.z = 0;
-
-// 	particles.vertices.push(vertex);
-// }
-
-
-			var scene = new THREE.Scene();
-// scene.background = new THREE.Color( 0xffffff );
-			var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
-			var renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
+// home 
+objLoader.load('https://cdn.glitch.com/6a29bec8-9471-403e-a502-375b8f18bbc2%2FStillettos_01.obj?v=1560178283848', function ( object ) {	
+	object.traverse( function ( child ) {
+		if ( child instanceof THREE.Mesh ) {
+			let scale = 2;
 			
-			const loader = new THREE.OBJLoader()
-			// loader.load('https://cdn.glitch.com/6a29bec8-9471-403e-a502-375b8f18bbc2%2Fbat%20tay.obj?v=1560150513467',
-			loader.load('https://cdn.glitch.com/6a29bec8-9471-403e-a502-375b8f18bbc2%2FBrain.obj?v=1560148572474',
-			// loader.load('https://cdn.glitch.com/c101cd3f-90ac-45dd-9a2a-b3b33d2d39be%2Fb5c36325324cf21518df524ac298eb5a.obj?v=1560109361856',
-			// loader.load('https://cdn.glitch.com/c101cd3f-90ac-45dd-9a2a-b3b33d2d39be%2FFree_Brain.obj?v=1560108459278',
-			// loader.load('https://cdn.glitch.com/c101cd3f-90ac-45dd-9a2a-b3b33d2d39be%2FAfrica_final.obj?v=1560108200642',
-			// loader.load('https://cdn.glitch.com/c101cd3f-90ac-45dd-9a2a-b3b33d2d39be%2FModesty_Veiled_Armchair.obj?v=1560108051253',
-			// loader.load('https://cdn.glitch.com/fcf3c007-b4eb-4250-ba6b-653fdab94ce3%2Fjapanese_temple.obj?1558792651869',
-			(obj) => {
-				// the request was successfull
-				let material = new THREE.PointsMaterial({ color: 'grey', size: 2 })
-				mesh = new THREE.Points(obj.children[0].geometry, material)
-				mesh.position.y = -85 //this model is not exactly in the middle by default so I moved it myself
-				// mesh.position.x = -100
-
-				scene.add(mesh)
-
-			},
-			(xhr) => {
-				// the request is in progress
-				console.log(xhr)
-			},
-			(err) => {
-				// something went wrong
-				console.error("loading .obj went wrong, ", err)
-			})
+			let area = new THREE.Box3();
+				area.setFromObject( child );
+			let yOffset = (area.max.y * scale) / 2;
 			
-			camera.position.z = 500;
-		
-			var animate = function () {
-				requestAnimationFrame( animate );
-				mesh.rotation.y += 0.001;
-				// mesh.rotation.y -= 0.01;
+			child.geometry.scale(scale,scale,scale);
+			homePoints = THREE.GeometryUtils.randomPointsInBufferGeometry(child.geometry, particleCount);
+			createVertices(homeParticles, homePoints, yOffset, 3);
+		}
+	});
+});
+// ABOUT ME
+objLoader.load('https://cdn.glitch.com/6a29bec8-9471-403e-a502-375b8f18bbc2%2FCamera.obj?v=1560149926567', function ( object ) {	
+	object.traverse( function ( child ) {
+		if ( child instanceof THREE.Mesh ) {
+			let scale = 2.5;
+			
+			let area = new THREE.Box3();
+				area.setFromObject( child );
+			let yOffset = (area.max.y * scale) / 2;
+			
+			child.geometry.scale(scale,scale,scale);
+			aboutPoints = THREE.GeometryUtils.randomPointsInBufferGeometry(child.geometry, particleCount);
+			createVertices(aboutParticles, aboutPoints, yOffset, 3);
+		}
+	});
+});
 
-				renderer.render( scene, camera );
-			};
+// Particles
+for (var p = 0; p < particleCount; p++) {
+	var vertex = new THREE.Vector3();
+			vertex.x = 0;
+			vertex.y = 0;
+			vertex.z = 0;
+
+	particles.vertices.push(vertex);
+}
+
+function createVertices (emptyArray, points, yOffset = 0, trigger = null) {
+	for (var p = 0; p < particleCount; p++) {
+		var vertex = new THREE.Vector3();
+				vertex.x = points[p]['x'];
+				vertex.y = points[p]['y'] - yOffset;
+				vertex.z = points[p]['z'];
+
+		emptyArray.vertices.push(vertex);
+	}
+	if (trigger !== null) {
+		triggers[trigger].setAttribute('data-disabled', false)
+	}
+}
+
+var particleSystem = new THREE.PointCloud(
+    particles,
+    pMaterial
+);
+
+particleSystem.sortParticles = true;
+
+// Add the particles to the scene
+scene.add(particleSystem);
+
+// Animate
+const normalSpeed = (defaultAnimationSpeed/100),
+			fullSpeed = (morphAnimationSpeed/100)
+
+let animationVars = {
+	speed: normalSpeed
+}
+
+function animate() {
+	stats.begin();
+	particleSystem.rotation.y += animationVars.speed;	
+	particles.verticesNeedUpdate = true; 
+	stats.end();
+	
+	window.requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+
+animate();
+setTimeout(toHome, 500);
+
+function toHome(){
+	handleTriggers(0);
+	morphTo(homeParticles);
+}
+
+function toAbout () {
+	handleTriggers(1);
+	morphTo(aboutParticles);
+}
+
+function toProject () {	
+	handleTriggers(2);
+	morphTo(projectParticles);
+}
+
+function toContact () {
+	handleTriggers(3);
+	morphTo(contactParticles);
+}
 
 
-			animate();
+function morphTo (newParticles, color = '0xffffff') {
+	TweenMax.to(animationVars, .3, {ease:
+	Power4.easeIn, speed: fullSpeed, onComplete: slowDown});
+	particleSystem.material.color.setHex(color);
+	
+	for (var i = 0; i < particles.vertices.length; i++){
+		TweenMax.to(particles.vertices[i], 4, {ease:
+	Elastic.easeOut.config( 1, 0.75), x: newParticles.vertices[i].x, y: newParticles.vertices[i].y, z: newParticles.vertices[i].z})
+	}
+}
+
+function slowDown () {
+	TweenMax.to(animationVars, 4, {ease:
+	Power2.easeOut, speed: normalSpeed, delay: 1});
+}
+
+triggers[0].addEventListener('click', toHome)
+triggers[1].addEventListener('click', toAbout)
+triggers[2].addEventListener('click', toProject)
+triggers[3].addEventListener('click', toContact)
+
+
+function handleTriggers (disable) {
+	for (var x = 0; x < triggers.length; x++) {
+		if (disable === x) {
+			triggers[x].setAttribute('data-disabled', true)		
+		} else {
+			triggers[x].setAttribute('data-disabled', false)
+		}
+	}	
+}
--- obj/ReadMe.txt
+++
@@ -1,7 +0,0 @@
-UFO concept- 4
-
-Model Specs:
-
-Vert: 666,434
-
-Faces: 666,402
